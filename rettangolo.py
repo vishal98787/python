bf002393cc417f7ab4db5a90af302c59f3052e7b
Vishal deep Singh <vishaldeep@MacBook-Air-di-Vishal.local>
2026-07-24 22:04:33 +0200

    Mio secondo commmit dal mac

---
+++ rettangolo.py
@@ -0,0 +1,6 @@
+base = float(input("Inserisci la base del rettangolo"))
+altezza = float(input("Inserisci l'altezza del rettangolo"))
+
+area = base * altezza
+
+print(f"un rettangolo con base {base} e altezza {altezza} ha un'area pari a {area}")
